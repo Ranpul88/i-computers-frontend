3fe391bc90e8329863b15099d8ec93816590790a
Ranpul88 <vithanagevinuja@gmail.com>
2025-11-10 19:26:06 +0530

    overvew page

--- pages/productOverview.jsx
+++ pages/productOverview.jsx
@@ -1,5 +1,75 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import toast from "react-hot-toast"
+import { useParams } from "react-router-dom"
+import Loader from "../src/components/loader"
+import ImageSlidder from "../src/components/imageSlidder"
+import { CgChevronRight } from "react-icons/cg"
+
 export default function ProductOverview() {
+
+    const params = useParams()
+    const [product, setProduct] = useState()
+    const [status, setStatus] = useState("loading")
+
+    const token = localStorage.getItem("token")
+
+    useEffect(()=>{
+        if(status == "loading"){
+            axios.get(import.meta.env.VITE_BACKEND_URL + "/products/" + params.productID, {
+                headers: {
+                    Authorization: "Bearer " + token
+                }
+            })
+                .then((response)=>{
+                    setProduct(response.data)
+                    setStatus("success")
+                }
+            )
+                .catch((error)=>{
+                    toast.error("Product Not Found.")
+                    console.log(error)
+                    setStatus("error")
+                }
+            )
+        }
+    }, [])
+
   return (
-    <div>productOverview</div>
+    <>
+        {
+            status == "loading" && <Loader /> 
+        }
+
+        {
+            status == "error" && <h1 className="text-center mt-10 text-2xl">Error Loading Product...</h1>
+        }
+
+        {
+            status == "success" && 
+            <div className="w-full h-[calc(100vh-100px)] flex">
+                <div className="w-1/2 h-full flex justify-center items-center">
+                    <ImageSlidder images={product.images}  />
+                </div>
+                <div className="w-1/2 h-full p-10 flex flex-col gap-6">
+                    <h1 className="text-4xl font-semibold">{product.name}</h1>
+                    <h2 className="text-lg text-secondary/80">{product.productID}</h2>
+                    <h3 className="text-lg text-secondary/80 flex items-center"><CgChevronRight /> {product.category}</h3>
+                    <p className="h-32 text-md text-justify text-secondary/90 overflow-y-auto">{product.description}</p>
+                    <div className="w-full ">
+                        {
+                            product.labelledPrice > product.price && 
+                            <h2 className="text-secondary/80 line-through decoration-gold/70 decoration-2 mr-2 text-xl">LKR. {product.labelledPrice.toFixed(2)}</h2>
+                        }
+                        <h2 className="text-accent font-semibold text-3xl">LKR. {product.price.toFixed(2)}</h2>
+                    </div>
+                    <div className="w-full  flex flex-row gap-4 mt-4">
+                        <button className="bg-accent text-white px-6 py-3 rounded-md hover:bg-accent/90 transition">Add to Cart</button>
+                        <button className="border-2 border-accent text-accent px-6 py-3 rounded-md hover:bg-accent hover:text-white">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        }
+    </>
   )
 }
--- src/components/imageSlidder.jsx
+++ src/components/imageSlidder.jsx
@@ -1,25 +1,23 @@
-import React from 'react'
+import { useState } from 'react'
 
 export default function ImageSlidder(props) {
 
-    const images = this.props.images
-    const [activeIndex, setActiveIndex] = React.useState(0)
+    const images = props.images
+    const [activeIndex, setActiveIndex] = useState(0)
 
   return (
-    <div className='w-full h-48 bg-red-900 flex flex-col items-center'>
-        <img src={images[activeIndex]} className='w-[80%] h-[500px] object-contain' />
-        <div className='w-full h-[100px] bg-accent flex flex-row justify-center gap-4'>
+    <div className='w-full flex flex-col items-center'>
+        <img src={images[activeIndex]} className='w-[60%] h-[500px] object-contain' />
+        <div className='w-full h-[100px] flex flex-row justify-center items-center gap-4'>
             {
-            images.map((image, index) => {
-                
-                return(
-                    <img src={images[index]} className={'w-[90px] h-[90px] object-cover rounded-lg' + ((activeIndex==index))?""}  onClick={()=>{
-                        setActiveIndex(index)
-                    }} />
-                )
-                }
-            )
-        }
+                images.map((image, index)=>{
+                    return(
+                        <img src={images[index]} className={'w-[90px] h-[90px] object-cover rounded-lg  cursor-pointer ' + ((activeIndex == index)&&'border-2 border-accent')} onClick={()=>{
+                            setActiveIndex(index)
+                        }} key={index} />
+                    )
+                })
+            }
         </div>
     </div>
   )
