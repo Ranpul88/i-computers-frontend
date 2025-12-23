ed132763cb41410850a7cf79759c6992fff68ce4
Ranpul88 <vithanagevinuja@gmail.com>
2025-12-23 07:20:50 +0530

    delete reviews ✏️`

--- pages/admin/adminUsersPage.jsx
+++ pages/admin/adminUsersPage.jsx
@@ -1,7 +1,5 @@
 import axios from "axios";
 import { useEffect, useState} from "react";
-import { BiPlus } from "react-icons/bi";
-import { Link } from "react-router-dom";
 import Loader from "../../src/components/loader";
 import { VscVerified } from "react-icons/vsc";
 
@@ -40,7 +38,7 @@ export default function AdminUsersPage() {
   return (
     <div className="w-full min-h-screen flex justify-center bg-primary text-secondary p-10 relative">
       <div className="w-full max-w-7xl bg-white rounded-2xl shadow-xl p-6 overflow-x-auto">
-        <h2 className="text-2xl font-semibold mb-6 text-accent border-b pb-3">Products List</h2>
+        <h2 className="text-2xl font-semibold mb-6 text-accent border-b pb-3">Users List</h2>
 
         {loaded ? <table className="w-full border-collapse text-sm">
           <thead className="bg-accent text-white sticky top-0">
@@ -81,13 +79,6 @@ export default function AdminUsersPage() {
           </tbody>
         </table> : <Loader />}
       </div>
-
-      <Link
-        to="/admin/add-product"
-        className="fixed right-8 bottom-8 w-[60px] h-[60px] flex justify-center items-center text-5xl bg-accent text-white rounded-full shadow-lg hover:bg-gold hover:shadow-xl transition-all duration-300"
-      >
-        <BiPlus />
-      </Link>
     </div>
   )
 }
