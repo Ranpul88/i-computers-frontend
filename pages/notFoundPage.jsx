35ed8ac96981e7ca230488e0eab179cc38d075c8
Ranpul88 <vithanagevinuja@gmail.com>
2025-12-24 22:26:42 +0530

    link to home page ✏️

--- pages/notFoundPage.jsx
+++ pages/notFoundPage.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function NotFoundPage() {
   return (
     <div className="w-full h-[calc(100vh-100px)] flex justify-center items-center gap-4 p-4">
         <div className="flex flex-col justify-center items-center gap-4 bg-white p-10 rounded-2xl shadow-lg">
             <h1 className="text-6xl font-bold text-accent">404</h1>
             <h2 className="text-2xl font-semibold text-accent">Page Not Found</h2>
-            <p className="text-center text-secondary">The page you are looking for does not exist or has been moved.</p>
+            <p className="text-center text-secondary mb-2">The page you are looking for does not exist or has been moved.</p>
+            <Link to="/" className="h-[40px] w-[100px] flex items-center justify-center bg-accent text-white rounded-md hover:bg-accent/90">Home Page</Link>
         </div>     
     </div>
   )
